86796e56c3e837118cf415052ff666b529cb6782
jpierrele <lejeanpierre@gmail.com>
2014-08-13 19:10:15 -0400

    Create ModuleSequence.java

---
+++ arche.lib/src/edu/cmu/sei/arche/ruleEngine/modules/ModuleSequence.java
@@ -0,0 +1,17 @@
+package edu.cmu.sei.arche.ruleengine.modules;
+
+public abstract class ModuleSequence implements Module {
+	
+	private void firstStateRule(){
+		
+	}
+	
+	private void intermediateStateRule(){
+		
+	}
+
+	private void lastStateRule(){
+		
+	}
+	
+}
